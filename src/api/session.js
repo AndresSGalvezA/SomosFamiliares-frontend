ff865e97f70ebc87e7a6f6fec38b2400626a72fc
Andres Gálvez <36344358+AndresSGalvezA@users.noreply.github.com>
2021-11-02 07:25:21 -0600

    Added upload imgs logic

---
+++ src/api/session.js
@@ -0,0 +1,141 @@
+import { IMAGE_PATH, BASE_PATH } from "./config";
+
+export function postImage(data) {
+    let date = new Date();
+    let fileName = date.getDate().toString() + (date.getMonth() + 1).toString() + date.getFullYear().toString() + date.getHours().toString() + date.getMinutes().toString() + date.getSeconds().toString() + date.getMilliseconds().toString();
+    const url = `${IMAGE_PATH}name=${fileName}.jpg`;
+    const params = {
+        method: "POST",
+        body: data
+    }
+
+    return fetch(url, params)
+    .then(response => {
+        return response.json();
+    })
+    .then(result => {
+        return result;
+    })
+    .catch(error => {
+        return {
+            status: 500,
+            message: "Error al cargar la imagen."
+        }
+    });
+}
+
+export function getFaceId(data) {
+    const url = `${BASE_PATH}/analisis/faceid`;
+    const params = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+
+    return fetch(url, params)
+    .then(response => {
+        return response.json();
+    })
+    .then(result => {
+        if (result.error) {
+            return 200;
+        } else {
+            return {
+                status: 500,
+                message: "Error al cargar la imagen."
+            }
+        }
+    })
+    .catch(error => {
+        return {
+            status: 500,
+            message: "Error al cargar la imagen."
+        }
+    });
+}
+
+export function getAnalysis(data) {
+    const url = `${BASE_PATH}/analisis/verify`;
+    const params = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+
+    return fetch(url, params)
+    .then(response => {
+        return response.json();
+    })
+    .then(result => {
+        if (result.error) {
+            return 200;
+        } else {
+            return {
+                status: 500,
+                message: "Error al cargar la imagen."
+            }
+        }
+    })
+    .catch(error => {
+        return {
+            status: 500,
+            message: "Error al cargar la imagen."
+        }
+    });
+}
+
+export function sendEmail(data) {
+    const url = `${BASE_PATH}/msg`;
+    const params = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+
+    return fetch(url, params)
+    .then(response => {
+        return response.json();
+    })
+    .then(result => {
+        if (result.error) {
+            return 200;
+        } else {
+            return {
+                status: 500,
+                message: "Error al cargar la imagen."
+            }
+        }
+    })
+    .catch(error => {
+        return {
+            status: 500,
+            message: "Error al cargar la imagen."
+        }
+    });
+}
+
+export function test() {
+    const url = `${BASE_PATH}`;
+    const params = {
+        method: "GET"
+    }
+
+    return fetch(url, params)
+    .then(response => {
+        return response.text();
+    })
+    .then(result => {
+        return result;
+    })
+    .catch(error => {
+        return {
+            message: error
+        }
+    });
+}
--- src/views/Test.js
+++ src/views/Test.js
@@ -1,9 +1,50 @@
 /* eslint-disable */
-import React from "react";
+import React, { useState } from "react";
+import { test, postImage } from "../api/session";
 import "../css/plugins.css";
 import "../css/style.css";
+import "../css/file-input.css";
+
+var data = new FormData();
+var img1, img2;
+
+const uploadImage = async e => {
+    const result1 = await postImage(img1);
+
+    if (result1.statusCode === 200) {
+        localStorage.setItem("img1", result.body);
+    } else {
+
+    }
+
+    //localStorage.getItem("img1");
+    const result2 = await postImage(img2);
+
+    if (result2.statusCode === 200) {
+        localStorage.setItem("img2", result.body);
+    }
+
+    //localStorage.getItem("img1");
+}
 
 export default function Test() {
+    const [image1, setImage1] = useState(null);
+    const [image2, setImage2] = useState(null);
+
+    const onImageChange1 = (e) => {
+        if (e.target.files && e.target.files[0]) {
+            img1 = e.target.files[0];
+            setImage1(URL.createObjectURL(e.target.files[0]));
+        }
+    }
+
+    const onImageChange2 = (e) => {
+        if (e.target.files && e.target.files[0]) {
+            img2 = e.target.files[0];
+            setImage2(URL.createObjectURL(e.target.files[0]));
+        }
+    }
+
     return (
         <div className="body-inner">
         <header id="header" data-fullwidth="true">
@@ -22,7 +63,7 @@ export default function Test() {
                         <div className="container">
                             <nav>
                                 <ul>
-                                    <li><a href="/">Sobre nosotros</a></li>
+                                    
                                 </ul>
                             </nav>
                         </div>
@@ -45,30 +86,33 @@ export default function Test() {
                     <div className="post-item border">
                         <div className="post-item-wrap">
                             <div className="post-image">
-                                <img alt="" src={require("../assets/img/12.jpg").default} />
-                                <span className="post-meta-category"><a href="/">Imagen 1</a></span>
+                                <img alt="Preview" src={image1 ? image1 : require("../assets/img/placeholder.png").default} />
+                                <span className="post-meta-category">Persona 1</span>
                             </div>
-                            <div className="post-item-description"> 
-                                <span className="post-meta-date"></span>
-                                <h2><a href="/">Carga de la imagen 1 </a></h2>
-                                <a href="/" className="item-link">Read More <i className="icon-chevron-right"></i></a>
+                            <div className="post-item-description">
+                                <label htmlFor="file-upload1" className="custom-file-upload">
+                                    Cargar imagen
+                                </label>
+                                <input id="file-upload1" type="file" accept=".jpeg" onChange={onImageChange1} />
                             </div>
                         </div>
                     </div>
                     <div className="post-item border">
                         <div className="post-item-wrap">
                             <div className="post-image">
-                                <img alt="" src="https://preview.redd.it/5mxa2ymcb3f11.png?auto=webp&s=9dd3927f07eedce826e4ff3e4b2d3c7a4b702a52" />
-                                <span className="post-meta-category"><a href="/">Imagen 1</a></span>
+                                <img alt="Preview" src={image2 ? image2 : require("../assets/img/placeholder.png").default} />
+                                <span className="post-meta-category">Persona 2</span>
                             </div>
                             <div className="post-item-description"> 
-                                <span className="post-meta-date"></span>
-                                <h2><a href="/">Carga de la imagen 1 </a></h2>
-                                <a href="/" className="item-link">Read More <i className="icon-chevron-right"></i></a>
+                                <label htmlFor="file-upload2" className="custom-file-upload">
+                                    Cargar imagen
+                                </label>
+                                <input id="file-upload2" type="file" accept=".jpeg" onChange={onImageChange2} />
                             </div>
                         </div>
                     </div>
                 </div>
+                <button className="btn btn-primary" onClick={uploadImage}>Hacer la prueba</button>
             </div>
         </section>
         <footer id="footer">
@@ -78,7 +122,7 @@ export default function Test() {
                         <div className="col-lg-5">
                             <div className="widget">
                                 <div className="widget-title">¿Somos familiares?</div>
-                                <p className="mb-5">Hecho desde Guatemala, Guatemala.<br/> Todos los derechos reservados.<br/> Derechos de autor © 2021. La Casa.</p>
+                                <p className="mb-5">Guatemala, Guatemala.<br/> Todos los derechos reservados.<br/> Derechos de autor © 2021. La Casa.</p>
                             </div>
                         </div>
                         <div className="col-lg-7">
